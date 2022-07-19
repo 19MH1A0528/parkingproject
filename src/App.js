bc8d6f112bcb12271e3e2e9bb566343ff8104b20
Renuka <69577791+19MH1A0528@users.noreply.github.com>
2022-07-19 10:23:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Slots from './Components/Slots'
+function App() {
+  return (
+    <div className='App'>
+        <Slots/>
+    </div>
+  );
+}
+
+export default App;
